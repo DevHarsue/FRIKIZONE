f696e78506024b79d5885a54469fea702ba3f4ac
loli11 <themilon.11@gmail.com>
2024-06-12 10:07:01 -0400

    en esta validacion esta comprueba
    1-nombres, sin espacios y sin minimo
    
    2-usuarios, minimo 8 caracteres, sin espacios
    alfanumerico,y puedes usar la barra _ de manera opcional
    
    3-contraseñas, minimo 8 caracteres,sin espacios,
    alfanumerico,minimo un letra en mayuscula, minimo tambien 2 caracteres
    espaciales
    
    4 -telefono ,esta desde el 0412 hasta el 0426 , esta implementado
    el +57 y el simbolo - esta implementado para usarse asi :
    
    0414-7581344
    +57-1099887766

--- Registro/Registro.py
+++ Registro/Registro.py
@@ -5,6 +5,7 @@ class Validar_registro():
         self.validar_string_regex = r'^[a-zA-Z]+$'
         self.users = r'^[a-zA-Z0-9_]{8,}$'
         self.Password = r'^(?=.*[A-Z])(?=.*[a-zA-Z0-9])(?=.*[@$!#%?&]{2,})[A-Za-z\d@$!#%?&]{8,}$'
+        self.num_telefono =r'^(0412|0414|0424|0416|0426)(-?\d{7}){1}$|^\+57(-?\d{10}){1}$'
 
     def validar_string(self, nombre):
         return bool(re.match(self.validar_string_regex, nombre))
@@ -14,3 +15,6 @@ class Validar_registro():
     
     def contraseña(self, password):
         return bool(re.match(self.Password, password))
+
+    def telefono(self,phone):
+        return bool(re.match(self.num_telefono,phone))
